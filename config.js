7f9600d877d43d325808dae50504601f84e48ac2
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2021-10-21 20:13:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBlccjLgHuPr3PVkIf2wCx_P9Xd2bm2Itg",
+  authDomain: "new-92-to-96-app.firebaseapp.com",
+  databaseURL: "https://new-92-to-96-app-default-rtdb.firebaseio.com",
+  projectId: "new-92-to-96-app",
+  storageBucket: "new-92-to-96-app.appspot.com",
+  messagingSenderId: "541622073582",
+  appId: "1:541622073582:web:ad9828a28c6a236fb8dbff"
+};
